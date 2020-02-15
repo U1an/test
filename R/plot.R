520d5b5bf56ce6418808555e89286ec8ddf480c0
Ulan <ulan139school_96@mail.ru>
2020-02-15 18:51:55 +0100

    plott

--- R/plot.R
+++ R/plot.R
@@ -1,8 +1,30 @@
 library(tidyverse)
 
-longest <- movies_df %>%
-  arrange(desc(Runtime))
-head(longest)
+mydata <- function() {
+  print(movies_df)
+}
 
-ggplot(movies_df,aes(x=Runtime,y=Rating))+
-  geom_point(aes(size=Votes,col=Genre))
+
+library('ggplot2')
+
+mypplot <- function(){
+  q<- qplot(data = movies_df,Runtime,fill = Genre,bins = 30)
+
+  return(q)
+}
+
+myggplot <- function(){
+
+  k<- ggplot(movies_df,aes(x=Runtime,y=Rating))+
+    geom_point(aes(size=Votes,col=Genre))
+
+  return(k)
+}
+
+mygplot <- function(){
+
+  s<-ggplot(movies_df,aes(x=Runtime,y=Gross_Earning_in_Mil))+
+    geom_point(aes(size=Rating,col=Genre))
+
+
+  return(s)}
